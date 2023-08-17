f659249f3cfda1a7d37f50135cb3d14d2a6f4b45
RyanLau0w0 <22023219@myrp.edu.sg>
2023-08-17 16:21:40 +0800

    RetriveActivityListView Completed

--- app/src/main/java/com/myapplicationdev/databaserevision/Note.java
+++ app/src/main/java/com/myapplicationdev/databaserevision/Note.java
@@ -1,6 +1,8 @@
 package com.myapplicationdev.databaserevision;
 
-public class Note {
+import java.io.Serializable;
+
+public class Note implements Serializable {
     private int id;
     private String content;
     private String priority;
--- app/src/main/java/com/myapplicationdev/databaserevision/RetrieveActivityListView.java
+++ app/src/main/java/com/myapplicationdev/databaserevision/RetrieveActivityListView.java
@@ -1,12 +1,16 @@
 package com.myapplicationdev.databaserevision;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
 
 import java.util.ArrayList;
 
@@ -29,6 +33,7 @@ public class RetrieveActivityListView extends AppCompatActivity {
         al = new ArrayList<>();
         aa = new ArrayAdapter<Note>(RetrieveActivityListView.this, android.R.layout.simple_list_item_1, al);
         lv.setAdapter(aa);
+        registerForContextMenu(lv);
 
         btnGetNotes.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -40,7 +45,25 @@ public class RetrieveActivityListView extends AppCompatActivity {
 
 
         //Option: Implement dialog to edit a record
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Note pos = al.get(position);
+                Intent i = new Intent(RetrieveActivityListView.this, InsertActivity.class);
+                i.putExtra("pos" , pos);
+                startActivity(i);
+            }
+        });
         //Option: Implement context to delete a record
-
+        lv.setOnItemLongClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                public void onClick(View v) {
+                    DBHelper dbh = new DBHelper(RetrieveActivityListView.this);
+                    dbh.deleteNote(al.getId());
+                    finish();
+                }
+            }
+        });
     }
 }
